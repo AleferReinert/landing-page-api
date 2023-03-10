e02e9640f5e9e85fade4fe00db01a74eb55d5e0c
Alefer Reinert <aleferreinert@gmail.com>
2023-03-10 14:23:13 -0300

    trying fix h10 error

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
   proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
   url: env("MY_HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
