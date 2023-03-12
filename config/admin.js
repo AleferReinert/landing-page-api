75a37f504ed94349577d40982c9c26681d704aab
Alefer Reinert <aleferreinert@gmail.com>
2023-03-12 16:17:31 -0300

    Update

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,14 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET"),
+    secret: env(
+      "ADMIN_JWT_SECRET",
+      "OBJOyZksIrpnCR3g8SgwaUOWemOdafuhIwPJXNrUqj1YyFoiQN4f+2Hfgo+kKxsN"
+    ),
   },
   apiToken: {
-    salt: env("API_TOKEN_SALT"),
+    salt: env(
+      "API_TOKEN_SALT",
+      "MYkOrDBhIqnHX5/e71D0xFqKZXIMWsslavvzUVh/vT7wfMiNxFXJKPukLHKQ7Wn0"
+    ),
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,9 +3,6 @@ module.exports = ({ env }) => ({
   host: "0.0.0.0",
   port: process.env.PORT,
   url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-  },
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
