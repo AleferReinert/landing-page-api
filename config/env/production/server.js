541a1959e89ef4fdd5f8b82b17ebe8a0f4b34f61
Alefer Reinert <aleferreinert@gmail.com>
2023-03-13 12:09:00 -0300

    Iniciado configurações para a Digital Ocean

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env("MY_HEROKU_URL"),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
+  url: env("APP_URL"), // Sets the public URL of the application.
+  app: {
+    keys: env.array("APP_KEYS"),
   },
 });
